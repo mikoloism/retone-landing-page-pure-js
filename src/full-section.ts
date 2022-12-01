2a37282f06e8c96784f770152554b71bac42b7f8
mikoloism <mikoloism@gmail.com>
2022-12-01 10:38:19 +0330

    feat(module): create `full-section` logics a module from the trash project;
    use for syncing with `anime.js` to handle scrolling (mouse wheel) and touch move to swipping the sections of landing page;

--- src/full-section.ts
+++ src/full-section.ts
@@ -1,16 +1,109 @@
+import { anime } from './lib';
+
 export class Range {
-	private minimum: number = 0;
-	private maximum: number = 1;
+	private static readonly MINIMUM: number = 0 as const;
+	private static MAXIMUM: number = 1 as const;
 
-	public includes(length: number): boolean {
-		return length < this.getMaximum() && length > this.getMinimum();
+	public static includes(length: number): boolean {
+		return length < Range.getMaximum() && length > Range.getMinimum();
 	}
 
-	public getMinimum(): number {
-		return this.minimum;
+	public static getMinimum(): number {
+		return Range.MINIMUM;
 	}
 
-	public getMaximum(): number {
-		return this.maximum;
+	public static getMaximum(): number {
+		return Range.MAXIMUM;
+	}
+
+	public static updateMaximum(value: number): void {
+		Range.MAXIMUM = value;
 	}
 }
+
+const TOUCH_THRESHOLD = 5;
+var isTransitionEnd = true;
+var currentAnimationIndex: number = 0;
+var currentAnimation: anime.AnimeInstance;
+
+var animationList: AnimationList = [];
+
+export function init(animations: AnimationList) {
+	animationList = animations;
+	window.addEventListener('wheel', handleOnMouseWheel, false);
+	window.addEventListener('touchstart', handleOnTouchStart, false);
+	window.addEventListener('touchend', handleOnTouchEnd, false);
+}
+
+function handleOnMouseWheel(event: WheelEvent): void {
+	triggerSwipe(event.deltaY);
+}
+
+var touchStartPoint: Point;
+
+function handleOnTouchStart(event: TouchEvent): void {
+	touchStartPoint = {
+		x: event.touches[0].clientX,
+		y: event.touches[0].clientY,
+	};
+}
+
+function handleOnTouchEnd(event: TouchEvent): void {
+	const touchEndPoint: Point = {
+		x: event.changedTouches[0].clientX,
+		y: event.changedTouches[0].clientY,
+	};
+
+	if (!shouldHandleTouches(touchStartPoint, touchEndPoint)) return;
+
+	const distanceY = touchStartPoint.y - touchEndPoint.y;
+	const distanceX = touchStartPoint.x - touchEndPoint.x;
+
+	triggerSwipe(distanceY - distanceX);
+}
+
+function shouldHandleTouches(startPoint: Point, endPoint: Point): boolean {
+	const distance = Math.sqrt(
+		Math.pow(startPoint.x - endPoint.x, 2) + Math.pow(startPoint.y - endPoint.y, 2)
+	);
+
+	return distance > TOUCH_THRESHOLD;
+}
+
+function triggerSwipe(direction: number): void {
+	if (!canTriggerSwipe()) return;
+
+	if (direction > 0) {
+		if (currentAnimationIndex > animationList.length) return;
+
+		currentAnimation = anime(animationList[currentAnimationIndex]);
+		isTransitionEnd = false;
+		currentAnimation.play();
+		currentAnimation.finished.then(() => {
+			isTransitionEnd = true;
+		});
+		currentAnimationIndex += 1;
+	} else if (direction < 0) {
+		if (currentAnimationIndex < 0) return;
+
+		currentAnimationIndex -= 1;
+		currentAnimation = anime(withReverseAnime(animationList[currentAnimationIndex]));
+		isTransitionEnd = false;
+		currentAnimation.play();
+		currentAnimation.finished.then(() => {
+			isTransitionEnd = true;
+		});
+	}
+}
+
+function canTriggerSwipe(): boolean {
+	return isTransitionEnd === true;
+}
+
+function withReverseAnime(animationObject: AnimationObject): anime.AnimeParams {
+	return { ...animationObject, direction: 'reverse' };
+}
+
+type Point = { x: number; y: number };
+export type AnimationObject = anime.AnimeParams;
+export type AnimationList = Array<AnimationObject>;
