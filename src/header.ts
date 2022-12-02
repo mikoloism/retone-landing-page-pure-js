3fd71f46cc40aaee0e33c7b65c64e256fa6e5be0
mikoloism <mikoloism@gmail.com>
2022-12-02 21:05:38 +0330

    fix(scripts): update `header` script;
    attach and detach `scroll event` (`mouse wheel event`) of`window` indepent to `sidebar` opened or closed state;

--- src/header.ts
+++ src/header.ts
@@ -1,4 +1,12 @@
+import { FullSection } from '../libs/FullSection/full-section';
+
 export namespace Header {
+	const enum HeaderState {
+		VISIBLE = 'header--visible',
+		BLURRED = 'header--blurred',
+	}
+	const SIDEBAR_VISIBLE: string = 'sidebar--visible';
+
 	var heroSectionObserver: IntersectionObserver;
 
 	export function init() {
@@ -9,29 +17,42 @@ export namespace Header {
 		});
 
 		const $hamburger = document.getElementById('hamburger')!;
-		const $navigation_close = document.getElementById('navigation-close')!;
+		const $sidebar_close_buttons = Array.from(
+			document.getElementsByClassName('sidebar__close')
+		);
 
-		$hamburger.addEventListener('click', toggleNavigation, false);
-		$navigation_close.addEventListener('click', toggleNavigation, false);
 		window.addEventListener('wheel', handleOnMouseWheel, false);
+		$hamburger.addEventListener('click', toggleSidebar, false);
+		$sidebar_close_buttons.forEach(($sidebar_close) => {
+			$sidebar_close!.addEventListener('click', toggleSidebar as any, false);
+		});
 	}
 
-	function handleHeroSectionObserve(entries: IntersectionObserverEntry[], _observer: any) {
+	function handleHeroSectionObserve(entries: IntersectionObserverEntry[], _observer: any): void {
 		const $header = document.getElementById('header')!;
 
-		if (entries[0].isIntersecting) return $header.classList.remove('header--blurred');
-		$header.classList.add('header--blurred');
+		if (entries[0].isIntersecting) return $header.classList.remove(HeaderState.BLURRED);
+		$header.classList.add(HeaderState.BLURRED);
 	}
 
-	function handleOnMouseWheel(event: WheelEvent) {
+	function handleOnMouseWheel(event: WheelEvent): void {
 		const $header = document.getElementById('header')!;
 		heroSectionObserver.observe(document.querySelector('.section--hero')!);
 
-		if (event.deltaY < 0) $header.classList.add('header--visible');
-		else if (event.deltaY > 0) $header.classList.remove('header--visible');
+		if (event.deltaY < 0) $header.classList.add(HeaderState.VISIBLE);
+		else if (event.deltaY > 0) $header.classList.remove(HeaderState.VISIBLE);
 	}
 
-	function toggleNavigation(_event: MouseEvent) {
-		document.getElementById('sidebar')!.classList.toggle('navigation--visible');
+	function toggleSidebar(_event: MouseEvent): void {
+		const $sidebar = document.getElementById('sidebar')!;
+
+		$sidebar.classList.toggle(SIDEBAR_VISIBLE);
+
+		if (isSidebarVisible()) FullSection.detachEventsListener();
+		else FullSection.attachEventsListener();
+
+		function isSidebarVisible(): boolean {
+			return $sidebar.className.indexOf(SIDEBAR_VISIBLE) != -1;
+		}
 	}
 }
