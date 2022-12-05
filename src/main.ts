082afeddf534f869ae763f6c021952573e40e878
mikoloism <mikoloism@gmail.com>
2022-12-05 14:58:21 +0330

    fix(scripts): add `footer` animation; fix the bugs; support on mobile sizes;
    add `animations` to anime.js animation list in small screen sizes; wrap `footer` nested children into `inner` element to add animation (for translateY not related to footer height);

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ const $_var = ProxyFactory(
 
 // const tagline = document.getElementById('tagline')!;
 const explode = document.getElementById('explode')! as HTMLVideoElement;
-const carouselAnimations: FullSection.AnimationList = [
+var carouselAnimations: FullSection.AnimationList = [
 	// @full-section-2 (cover)
 	{
 		targets: '.fs__wrapper',
@@ -216,6 +216,20 @@ const carouselAnimations: FullSection.AnimationList = [
 			'calc(var(--view-height, 1vh) * -400)',
 			'calc(var(--view-height, 1vh) * -500)',
 		],
+		begin() {
+			let $taglineVideo = document.querySelector<HTMLVideoElement>('#tagline-video')!;
+
+			anime({
+				targets: $taglineVideo,
+				volume: [1, 0],
+				easing: 'easeInOutQuad',
+				duration: 1000,
+				autoplay: true,
+			}).finished.then(() => {
+				$taglineVideo.pause();
+				$taglineVideo.volume = 1;
+			});
+		},
 	},
 
 	// @full-section-7 (story)
@@ -242,23 +256,54 @@ const carouselAnimations: FullSection.AnimationList = [
 
 	// @footer-visible
 	{
-		targets: '.fs__wrapper',
+		targets: '#fs-wrapper',
 		easing: 'easeInOutQuad',
 		duration: 1000,
-		top: [
-			'0px',
-			`-${document.querySelector('.footer')!.getBoundingClientRect().height - 50}px`,
-		],
+		top: ['0px', `-50vh`],
+	},
+];
+
+const additionalCarouselAnimations: FullSection.AnimationList = [
+	// @footer-visible
+	{
+		targets: '#fs-wrapper',
+		easing: 'easeInOutQuad',
+		duration: 1000,
+		top: [`-50vh`, '-100vh'],
 
 		begin(anim) {
 			anime({
-				targets: '.footer',
-				easing: 'linear',
-				duration: 400,
-				autoplay: true,
+				targets: '#footer',
+				easing: 'easeInOutQuad',
+				duration: 700,
+				height: [`calc(50vh + 50px)`, `100vh`],
 				direction: anim.direction,
+				autoplay: true,
+			});
+
+			anime({
+				targets: '#footer-fs-wrapper',
+				easing: 'easeInOutQuad',
+				duration: 1000,
+				translateY: [`calc(-50vh - 50px)`, '-33.33%'],
+				direction: anim.direction,
+				autoplay: true,
 			});
 		},
+	},
+
+	{
+		targets: '#footer-fs-wrapper',
+		easing: 'easeInOutQuad',
+		duration: 1000,
+		translateY: ['-33.33%', '-66.66%'],
+	},
+
+	{
+		targets: '#footer-fs-wrapper',
+		easing: 'easeInOutQuad',
+		duration: 1000,
+		translateY: ['-66.66%', '-99.99%'],
 	},
 ];
 
@@ -266,10 +311,14 @@ const carouselAnimations: FullSection.AnimationList = [
 	FullSection.init(carouselAnimations);
 	Header.init();
 
-	ViewSize.onUpdateScreen(({ realScreenHeight }) => {
+	ViewSize.onUpdateScreen(({ realScreenHeight, realScreenWidth }) => {
 		document.getElementById('museum')!.style.transform = `translate(-50%, -50%) scale(${
 			realScreenHeight / 1080
 		})`;
+
+		if (realScreenWidth < 425) {
+			carouselAnimations.push(...additionalCarouselAnimations);
+		}
 	});
 	ViewSize.init();
 })();
