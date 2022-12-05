7aaa3c6918e88752df208a688abe2f8bf5abfb65
mikoloism <mikoloism@gmail.com>
2022-12-05 15:24:20 +0330

    build(configs): add pages to `vite` build configs;

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				about: resolve(__dirname, "/pages/about/index.html"),
+				solutions: resolve(__dirname, "pages/solutions/index.html"),
+				story: resolve(__dirname, "pages/story/index.html"),
+			},
+		},
+	},
 	css: {
 		preprocessorOptions: {
 			scss: {
