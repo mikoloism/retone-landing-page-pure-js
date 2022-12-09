7bd016dc6eca5d659880afbe62b131eefc0e82cf
mikoloism <mikoloism@gmail.com>
2022-12-09 14:25:03 +0330

    fix(pages): update `solutions` page;
    fix stricky header with disable `auto-hide` by sticky position in style; fix scrollable section with `intersection observer api` in javascript; extract `paragraphs` from the `section header` to inside of the `section`; fix solution relative path to `main.ts` of the script the page;

--- pages/solutions/main.ts
+++ pages/solutions/main.ts
@@ -1,3 +1,4 @@
+import { Utils } from "../../libs/FullSection/refactored";
 import { FullSection } from "../../libs/FullSection/full-section";
 import { Header } from "../../src/header";
 import { ViewSize } from "../../src/view-size";
@@ -13,19 +14,43 @@ const carouselAnimations: FullSection.AnimationList = [
 		targets: "#fs-wrapper",
 		easing: "easeInOutQuad",
 		duration: 1000,
-		translateY: ["-25%", "-50%"],
+		translateY: [`-25%`, `-50%`],
 	},
 	{
 		targets: "#fs-wrapper",
 		easing: "easeInOutQuad",
 		duration: 1000,
-		translateY: ["-50%", "-75%"],
+		translateY: [`-50%`, `-75%`],
 	},
 ];
 
 (function startup() {
+	const observer = new IntersectionObserver(handleObserver);
+	observer.observe(document.querySelector<HTMLDivElement>("#fs-section-1")!);
+	observer.observe(document.querySelector<HTMLDivElement>("#fs-section-2")!);
+	observer.observe(document.querySelector<HTMLDivElement>("#fs-section-3")!);
+	observer.observe(document.querySelector<HTMLDivElement>("#fs-section-4")!);
+
 	ViewSize.init();
+
 	FullSection.init(carouselAnimations);
+
 	Header.disableObserver();
 	Header.init();
+	Header.detachEventsListener();
 })();
+
+function handleObserver(entries: IntersectionObserverEntry[], _observer: any): void {
+	entries.forEach((entry: IntersectionObserverEntry) => {
+		let $self = entry.target as HTMLDivElement;
+
+		if (entry.isIntersecting) {
+			FullSection.detachEventsListener();
+			$self.addEventListener("wheel", handleSwipe.bind(null, $self));
+			$self.addEventListener("touchmove", handleSwipe.bind(null, $self));
+		}
+	});
+}
+function handleSwipe($self: HTMLDivElement) {
+	if (Utils.isScrollEnd($self)) FullSection.attachEventsListener();
+}
