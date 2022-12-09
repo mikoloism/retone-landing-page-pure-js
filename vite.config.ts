5ee79ca877194d135c6f2db0bc9ba10ebf00a9d2
mikoloism <mikoloism@gmail.com>
2022-12-09 11:03:32 +0330

    fix(config): update `tsconfig.json` and `vite.config.ts`;
    add `index.html` from `pages/` directory to `rollup options`; format the file to remove spaces; fix to add slash before of the paths to resolve;

--- vite.config.ts
+++ vite.config.ts
@@ -12,20 +12,13 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, "index.html"),
+				main: resolve(__dirname, "/pages/index.html"),
 				about: resolve(__dirname, "/pages/about/index.html"),
-				solutions: resolve(__dirname, "pages/solutions/index.html"),
-				story: resolve(__dirname, "pages/story/index.html"),
+				solutions: resolve(__dirname, "/pages/solutions/index.html"),
+				story: resolve(__dirname, "/pages/story/index.html"),
 			},
 		},
 	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				quietDeps: true,
-			},
-		},
-	},
-
+	css: { preprocessorOptions: { scss: { quietDeps: true } } },
 	plugins: [SVGLoadingComponent.setup(), HeaderComponent.setup(), SidebarComponent.setup()],
 });
