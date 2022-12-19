ba6a154105be5fbcc6a5c4ce61c7dca11277665b
mikoloism <mikoloism@gmail.com>
2022-12-19 13:19:10 +0330

    fix(scripts): update animation list of `blog`, `story` and `solutions` page script;

--- pages/solutions/main.ts
+++ pages/solutions/main.ts
@@ -3,30 +3,36 @@ import { Header } from "../../src/header";
 import { ViewSize } from "../../src/view-size";
 
 const carouselAnimations: FullSection.AnimationList = [
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: ["0", "-25%"],
-	},
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: [`-25%`, `-50%`],
-	},
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: [`-50%`, `-75%`],
-	},
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: ["0", "-25%"],
+		},
+	],
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: [`-25%`, `-50%`],
+		},
+	],
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: [`-50%`, `-75%`],
+		},
+	],
 ];
 
 (function startup() {
 	ViewSize.init();
 
-	FullSection.init(carouselAnimations);
+	FullSection.init(carouselAnimations, { 0: 1, 1: 2, 2: 3, 3: 4 });
 
 	Header.disableObserver();
 	Header.init();
--- pages/story/main.ts
+++ pages/story/main.ts
@@ -3,54 +3,77 @@ import { Header } from "../../src/header";
 import { ViewSize } from "../../src/view-size";
 
 const carouselAnimations: FullSection.AnimationList = [
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: ["0", "-12.5%"],
-	},
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: ["-12.5%", "-25%"],
-	},
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: ["-25%", "-37.5%"],
-	},
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: ["-37.5%", "-50%"],
-	},
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: ["-50%", "-62.5%"],
-	},
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: ["-62.5%", "-75%"],
-	},
-	{
-		targets: "#fs-wrapper",
-		easing: "easeInOutQuad",
-		duration: 1000,
-		translateY: ["-75%", "-87.5"],
-	},
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: ["0", "-12.5%"],
+		},
+	],
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: ["-12.5%", "-25%"],
+		},
+	],
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: ["-25%", "-37.5%"],
+		},
+	],
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: ["-37.5%", "-50%"],
+		},
+	],
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: ["-50%", "-62.5%"],
+		},
+	],
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: ["-62.5%", "-75%"],
+		},
+	],
+	[
+		{
+			targets: "#fs-wrapper",
+			easing: "easeInOutQuad",
+			duration: 1000,
+			translateY: ["-75%", "-87.5"],
+		},
+	],
 ];
 
 (function startup() {
 	ViewSize.init();
 
-	FullSection.init(carouselAnimations);
+	FullSection.init(carouselAnimations, {
+		0: 1,
+		1: 2,
+		2: 3,
+		3: 4,
+		4: 5,
+		5: 6,
+		6: 7,
+		7: 8,
+	});
 
 	Header.disableObserver();
 	Header.init();
