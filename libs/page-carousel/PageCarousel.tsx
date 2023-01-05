72040d435d0bde864664debfbb3a0aee4ff63691
mikoloism <mikoloism@gmail.com>
2023-01-05 09:55:39 +0330

    feat(libs): add `page-carousel` logic class to `page-carousel` component class;

--- libs/page-carousel/PageCarousel.tsx
+++ libs/page-carousel/PageCarousel.tsx
@@ -5,8 +5,19 @@ import { PageCarousel } from './page-carousel';
 import styled from './page-carousel.module.scss';
 
 export class PageCarouselComponent extends React.Component<Props, State> {
+	private readonly pageCarousel: PageCarousel;
+
 	public constructor(props: Props) {
 		super(props);
+		this.pageCarousel = new PageCarousel();
+	}
+
+	public componentDidMount(): void {
+		this.pageCarousel.attachEvents();
+	}
+
+	public componentWillUnmount(): void {
+		this.pageCarousel.detachEvents();
 	}
 
 	public render(): JSX.Element {
