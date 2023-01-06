e1c3bf6811a8e933e094acb4f03837b87ca25515
mikoloism <mikoloism@gmail.com>
2023-01-06 11:10:05 +0330

    perf(libs): add `bound` method to `page-carousel` logic class and more update;
    make binding in class by `bound` method; fix dual event fire by setimeout of animation duration (rename to constant variable); seperate `playAnimation` from `swipe` method (`SRP`); make all constructor initialzed variables `readonly` flag; re-order `static` methods to the down side of page carousel class;

--- libs/page-carousel/page-carousel.ts
+++ libs/page-carousel/page-carousel.ts
@@ -2,71 +2,49 @@ import { Direction, Point } from '@/libs/direction';
 import { Iterator } from '@/libs/iteratify';
 
 export class PageCarousel {
-	private direction: Direction = new Direction();
-	private animations: Iterator.TReturn<any>;
+	private readonly ANIMATION_DURATION: number;
+	private readonly direction: Direction;
+	private readonly animations: Iterator.Iteratee<any>;
+	private readonly updateAnimation: Function;
 
-	public constructor() {
-		this.animations = Iterator.createIterator<any>([]);
+	public constructor(props: Props) {
+		this.direction = new Direction();
+		this.animations = Iterator.createIterator<any>(props.animationList);
+
+		// TODO : give constant from the argument of constructor from invoked
+		this.ANIMATION_DURATION = 500;
+		this.updateAnimation = props.stateUpdater;
+
+		this.bound.call(this);
 	}
 
-	private static isEnabled: boolean = true;
-
-	public static disable(): void {
-		PageCarousel.isEnabled = false;
-	}
-
-	public static enable(): void {
-		PageCarousel.isEnabled = true;
-	}
-
-	private isEnabled(): boolean {
-		return PageCarousel.isEnabled;
+	private bound(): void {
+		this.attachEvents = this.attachEvents.bind(this);
+		this.detachEvents = this.detachEvents.bind(this);
+		this._handleKeyUp = this._handleKeyUp.bind(this);
+		this._handleTouchStart = this._handleTouchStart.bind(this);
+		this._handleTouchEnd = this._handleTouchEnd.bind(this);
+		this._handleMouseWheel = this._handleMouseWheel.bind(this);
+		this.swipe = this.swipe.bind(this);
+		this.playAnimation = this.playAnimation.bind(this);
 	}
 
 	public attachEvents(): void {
 		if (typeof window == undefined) return;
 
-		window.addEventListener('keyup', this._handleKeyUp.bind(this), false);
-		window.addEventListener(
-			'touchstart',
-			this._handleTouchStart.bind(this),
-			false
-		);
-		window.addEventListener(
-			'touchend',
-			this._handleTouchEnd.bind(this),
-			false
-		);
-		window.addEventListener(
-			'wheel',
-			this._handleMouseWheel.bind(this),
-			false
-		);
+		window.addEventListener('keyup', this._handleKeyUp, false);
+		window.addEventListener('touchstart', this._handleTouchStart, false);
+		window.addEventListener('touchend', this._handleTouchEnd, false);
+		window.addEventListener('wheel', this._handleMouseWheel, false);
 	}
 
 	public detachEvents(): void {
 		if (typeof window == undefined) return;
 
-		window.removeEventListener(
-			'keyup',
-			this._handleKeyUp.bind(this),
-			false
-		);
-		window.removeEventListener(
-			'touchstart',
-			this._handleTouchStart.bind(this),
-			false
-		);
-		window.removeEventListener(
-			'touchend',
-			this._handleTouchEnd.bind(this),
-			false
-		);
-		window.removeEventListener(
-			'wheel',
-			this._handleMouseWheel.bind(this),
-			false
-		);
+		window.removeEventListener('keyup', this._handleKeyUp, false);
+		window.removeEventListener('touchstart', this._handleTouchStart, false);
+		window.removeEventListener('touchend', this._handleTouchEnd, false);
+		window.removeEventListener('wheel', this._handleMouseWheel, false);
 	}
 
 	private _handleMouseWheel(event: WheelEvent): void {
@@ -112,14 +90,49 @@ export class PageCarousel {
 		return;
 	}
 
+	private isPlaying: boolean = false;
 	private swipe(): void {
 		this.direction.compute();
 
+		if (this.isPlaying) return;
+
 		if (this.direction.is(Direction.Direct.Up)) {
-			this.animations.next();
+			this.playAnimation(this.animations.next());
 		} else if (this.direction.is(Direction.Direct.Down)) {
-			this.animations.prev();
+			this.playAnimation(this.animations.prev());
 		}
+	}
+
+	private playAnimation(iteratee: Iterator.TResult<any>): void {
+		let timeout: number;
+		const executor = (resolve: any, reject: any) => {
+			try {
+				this.isPlaying = true;
+				this.updateAnimation(iteratee.value);
+				timeout = window.setTimeout(
+					() => resolve(),
+					this.ANIMATION_DURATION
+				);
+			} catch (error: any) {
+				reject();
+			}
+		};
+
+		new Promise(executor)
+			.then(() => (this.isPlaying = false))
+			.then(() => timeout && window.clearTimeout(timeout))
+			.catch(() => window.alert('Error'));
+	}
+
+	private static isEnabled: boolean = true;
+	public static disable(): void {
+		PageCarousel.isEnabled = false;
+	}
+	public static enable(): void {
+		PageCarousel.isEnabled = true;
+	}
+	private isEnabled(): boolean {
+		return PageCarousel.isEnabled;
 	}
 }
 
@@ -129,3 +142,8 @@ const enum Key {
 	PageUp = 'pageup',
 	PageDown = 'pagedown',
 }
+
+type Props = {
+	stateUpdater: Function;
+	animationList: string[];
+};
