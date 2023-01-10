fa32fdc7ea833b9dd7d6e59cbf910ab018407e94
mikoloism <mikoloism@gmail.com>
2023-01-10 16:36:43 +0330

    feat(components): support `fullscreen` api by `use-fullscreen` hook in `tagline` section;
    when video playing, going to fullscreen, and if paused video going to un-fullscreen and if was un-fullscreen, going to paused;

--- components/Section/Tagline.tsx
+++ components/Section/Tagline.tsx
@@ -1,15 +1,14 @@
+import { useContext as usePageCarouselContext } from '@/libs/page-carousel';
+import { useFullscreen } from '@/libs/use-fullscreen';
 import { PropsWithIntersection } from '@/libs/use-intersection/use-intersection';
+import React from 'react';
 import styled from 'styled-components';
 import { MediaSection } from './styled';
 
 export function TaglineComponent(_props: Props): JSX.Element {
 	return (
 		<Tagline.Wrapper>
-			<Tagline.Video
-				poster="/assets/tagline/poster.jpg"
-				src="/assets/tagline/video.mp4"
-				controls
-			/>
+			<TaglineVideo />
 		</Tagline.Wrapper>
 	);
 }
@@ -17,9 +16,10 @@ export function TaglineComponent(_props: Props): JSX.Element {
 type Props = PropsWithIntersection;
 
 namespace Tagline {
-	export const Video = styled.video`
+	export const Video = styled.video<{ $isFullscreen: boolean }>`
 		position: absolute;
 		z-index: 1;
+		object-fit: ${(props) => (props.$isFullscreen ? 'contain' : 'cover')};
 	`;
 
 	export const Wrapper = styled(MediaSection)`
@@ -31,4 +31,70 @@ namespace Tagline {
 			object-fit: cover;
 		}
 	`;
+}
+
+function TaglineVideo() {
+	const videoRef = React.useRef<HTMLVideoElement>(null);
+	const pageCarousel = usePageCarouselContext();
+	const { ref, isFullscreen, enterFullscreen, exitFullscreen } =
+		useFullscreen({
+			onEnter() {
+				pageCarousel?.disable();
+			},
+
+			onExit() {
+				if (!videoRef.current) return;
+
+				if (isVideoPlaying(videoRef)) {
+					videoRef.current?.pause();
+					videoRef.current.currentTime = 0;
+					pageCarousel?.enable();
+				}
+			},
+		});
+
+	const handlePlaying = () => {
+		if (isFullscreen) return;
+
+		enterFullscreen();
+	};
+
+	const handlePause = () => {
+		if (!isFullscreen) return;
+
+		exitFullscreen().then(() => {
+			if (isVideoPlaying(videoRef)) {
+				videoRef.current?.pause();
+				videoRef.current!.currentTime = 0;
+				pageCarousel?.enable();
+			}
+		});
+	};
+
+	React.useEffect(() => {
+		ref(videoRef.current);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [videoRef.current]);
+
+	return (
+		<Tagline.Video
+			ref={videoRef}
+			$isFullscreen={isFullscreen}
+			onPlay={handlePlaying}
+			onPause={handlePause}
+			poster="/assets/tagline/poster.jpg"
+			src="/assets/tagline/video.mp4"
+			controls
+		/>
+	);
+}
+
+function isVideoPlaying(video: React.RefObject<HTMLVideoElement>): boolean {
+	if (!video.current) return false;
+	return !!(
+		video.current.currentTime > 0 &&
+		!video.current.paused &&
+		!video.current.ended &&
+		video.current.readyState > 2
+	);
 }
