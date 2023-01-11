5eba4f009bcac7e0dbbda5be1ba71f3023602354
mikoloism <mikoloism@gmail.com>
2023-01-11 22:14:28 +0330

    fix(components): update `blog` section component;
    remove duplicate content; replace `strong` element instead `p` element on styled-components;

--- components/Section/Blog.tsx
+++ components/Section/Blog.tsx
@@ -15,7 +15,7 @@ export function BlogComponent(props: Props): JSX.Element {
 
 				<Blog.SubTitle>
 					<SplitText play={entry?.isIntersecting}>
-						Best Door In The World !!!Best Door In The World !!!
+						Best Door In The World !!!
 					</SplitText>
 				</Blog.SubTitle>
 			</Blog.Header>
@@ -26,29 +26,31 @@ export function BlogComponent(props: Props): JSX.Element {
 type Props = PropsWithIntersection;
 
 namespace Blog {
-	export const Wrapper = styled(TextSection)``;
+	export const Wrapper = styled(TextSection)`
+		background-color: gray;
+	`;
 
 	export const Header = styled.header`
 		display: flex;
 		min-width: 250px;
 		width: 100%;
 		padding: 0 10%;
-
 		flex-direction: column;
 		flex-wrap: wrap;
-
 		place-items: flex-start;
 		justify-content: center;
 		row-gap: 1rem;
-
+		color: black;
 		line-height: 1.35;
 	`;
 
 	export const Title = styled.h2`
 		font-size: viewSize(3.3);
+		font-size: 3.3rem;
 	`;
 
-	export const SubTitle = styled.p`
+	export const SubTitle = styled.strong`
 		font-size: viewSize(6);
+		font-size: 6rem;
 	`;
 }
